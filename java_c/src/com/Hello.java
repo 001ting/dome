e57e777009f73e518ad936badb2415c62fc7f16a
001ting <761409630@qq.com>
2018-01-03 17:41:08 +0800

    zz

--- java_c/src/com/Hello.java
+++ java_c/src/com/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("哈是的撒事实上");
 		System.out.println("哈是的撒事实上11111");
 		System.out.println("哈是的撒事实上11111");
-		System.out.println("t666666666666");
+		System.out.println("t66666666666s6");
 		System.out.println("tt666666666666");
 	}
 }
