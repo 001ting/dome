ede4938ea9116eda713d7e16bde5d4f72f11547d
001ting <761409630@qq.com>
2018-01-03 17:20:21 +0800

    提交

---
+++ java_c/src/com/Hello.java
@@ -0,0 +1,12 @@
+package com;
+
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("ÄãºÃ£¡£¡£¡");
+		System.out.println("¹þ¹þ£¡£¡£¡");
+		System.out.println("¹þ¹þ£¡£¡£¡");
+		System.out.println("¹þ¹þ£¡£¡£¡");
+		System.out.println("¹þÊÇµÄÈö");
+	}
+}
